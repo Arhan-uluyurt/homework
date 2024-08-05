dedbcae7da9e6fee9cc0458e12f340f0ff1c0c1d
Arhan-uluyurt <aberration00x@gmail.com>
2024-08-05 22:45:30 +0300

    Add files via upload

---
+++ homework1/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("üç sayı giriniz.");
+
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+                
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            
+            int sayi3 = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi1 == sayi2 && sayi2 == sayi3)
+            {
+                Console.WriteLine(sayi1 + "=" + sayi2 + "=" + sayi3);
+            }
+
+            else if (sayi1 >= sayi2 && sayi1 >= sayi3)
+            {
+                if (sayi1 == sayi2)
+                {
+                    Console.WriteLine(sayi1 + "=" + sayi2 + ">" + sayi3);
+                }
+                else if (sayi2 > sayi3)
+                {
+                    Console.WriteLine(sayi1 + ">" + sayi2 + ">" + sayi3);
+                }
+                else if (sayi2 == sayi3)
+                {
+                    Console.WriteLine(sayi1 + ">" + sayi2 + "=" + sayi3);
+                }
+                else if (sayi1 == sayi3)
+                {
+                    Console.WriteLine(sayi1 + "=" + sayi3 + ">" + sayi2);
+                }
+                else
+                {
+                    Console.WriteLine(sayi1 + ">" + sayi3 + ">" + sayi2);
+                }
+            }
+
+            else if (sayi2 >= sayi1 && sayi2 >= sayi3)
+            {
+                if (sayi2 == sayi1)
+                {
+                    Console.WriteLine(sayi2 + "=" + sayi1 + ">" + sayi3);
+                }
+                else if (sayi1 > sayi3)
+                {
+                    Console.WriteLine(sayi2 + ">" + sayi1 + ">" + sayi3);
+                }               
+                else if (sayi1 == sayi3)
+                {
+                    Console.WriteLine(sayi2 + ">" + sayi1 + "=" + sayi3);
+                }
+                else if (sayi2 == sayi3)
+                {
+                    Console.WriteLine(sayi2 + "=" + sayi3 + ">" + sayi1);
+                }
+                else
+                {
+                    Console.WriteLine(sayi2 + ">" + sayi3 + ">" + sayi1);
+                }
+            }
+
+            else if (sayi3 >= sayi1 && sayi3 >= sayi2)
+            {
+                if (sayi1 > sayi2)
+                {
+                    Console.WriteLine(sayi3 + ">" + sayi1 + ">" + sayi2);
+                }
+                else if (sayi1 == sayi2)
+                {
+                    Console.WriteLine(sayi3 + ">" + sayi1 + "=" + sayi2);
+                }
+                else if (sayi3 == sayi2)
+                {
+                    Console.WriteLine(sayi3 + "=" + sayi2 + ">" + sayi1);
+                }
+                else
+                {
+                    Console.WriteLine(sayi3 + ">" + sayi2 + ">" + sayi1);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("geçerli bir sayı giriniz.");
+            }
+        }
+    }
+}
