4adbe57e778668513a31cca465fe68a350c81dca
Arhan-uluyurt <aberration00x@gmail.com>
2024-08-18 00:09:26 +0300

    Add files via upload

---
+++ homework3/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace homework3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 0;
+            int sayi2 = 0;
+
+
+            while (true)
+            {
+                Console.Write("Birinci sayıyı girin: ");
+                try
+                {
+                    sayi1 = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Geçersiz giriş. Geçerli bir sayı girin.");
+                }
+            }
+            while (true)
+            {
+                Console.Write("İkinci sayıyı girin: ");
+                try
+                {
+                    sayi2 = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Geçersiz giriş. Geçerli bir sayı girin.");
+                }
+            }
+
+            int toplam = sayi1 + sayi2;
+            Console.WriteLine("Toplam: " + toplam);
+        }
+
+    }
+}
+
